15af8e86b9cf4dce1595e8a92feec1e883028f2b
pangpangE123 <eric0420@naver.com>
2024-04-05 16:08:58 +0900

    push1

--- src/main/java/com/nhnacademy/server1/Server1Application.java
+++ src/main/java/com/nhnacademy/server1/Server1Application.java
@@ -2,12 +2,14 @@ package com.nhnacademy.server1;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
+@EnableConfigServer
 public class Server1Application {
 
-	public static void main(String[] args) {
-		SpringApplication.run(Server1Application.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(Server1Application.class, args);
+  }
 
 }
--- src/test/java/com/nhnacademy/server1/Server1ApplicationTests.java
+++ src/test/java/com/nhnacademy/server1/Server1ApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class Server1ApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+  @Test
+  void contextLoads() {
+  }
 
 }
